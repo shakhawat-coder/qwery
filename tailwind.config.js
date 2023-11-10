440e98330ab6d34a18d7aa9da8708b770857f279
shakhawat <shakhawathossen188@gmail.com>
2023-11-10 23:05:19 +0600

    navbar design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
-      
+      maxWidth: {
+        
+      },
+      colors:{
+        navcolour:"#ffffff99",
+      },
+      fontFamily:{
+        'opensans': "'Open Sans', sans-serif;",
+      },
+      backgroundImage: {
+        'bannerbg': "url('./images/banner.jpg')",
+         'startbg': "url('./images/cta.png')",
+      },
     },
   },
   plugins: [],
